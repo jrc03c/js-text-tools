7ad4a17dd2e557db61775e6046c88cf346169be1
jrc03c <jrc03c@pm.me>
2023-06-13 17:11:24 -0500

    ...

--- src/stringify.js
+++ src/stringify.js
@@ -1,7 +1,92 @@
 const { decycle } = require("@jrc03c/js-math-tools")
 
-function stringify(x, replacer, space) {
-  return JSON.stringify(decycle(x), replacer, space)
+// 0,
+// 1,
+// 2.3,
+// -2.3,
+// Infinity,
+// -Infinity,
+// NaN,
+// "foo",
+// true,
+// false,
+// null,
+// undefined,
+// Symbol.for("Hello, world!"),
+// [2, 3, 4],
+// [
+//   [2, 3, 4],
+//   [5, 6, 7],
+// ],
+// x => x,
+// function (x) {
+//   return x
+// },
+// { hello: "world" },
+// selfReferencer,
+
+function stringify(x, prefix) {
+  function helper(x, prefix) {
+    if (typeof x === "number" || typeof x === "bigint") {
+      if (x === Infinity) {
+        return "Infinity"
+      }
+
+      if (x === -Infinity) {
+        return "-Infinity"
+      }
+
+      if (isNaN(x)) {
+        return "NaN"
+      }
+
+      return x.toString()
+    }
+
+    if (typeof x === "string") {
+      return JSON.stringify(x)
+    }
+
+    if (typeof x === "boolean") {
+      return x.toString()
+    }
+
+    if (typeof x === "undefined") {
+      return "undefined"
+    }
+
+    if (typeof x === "symbol") {
+      return x.toString()
+    }
+
+    if (typeof x === "function") {
+      return x.toString()
+    }
+
+    if (typeof x === "object") {
+      if (x === null) {
+        return "null"
+      }
+
+      if (x.map) {
+        return "[" + x.map(v => helper(v, prefix)).join(", ") + "]"
+      }
+
+      return (
+        "{" +
+        Object.keys(x)
+          .map(key => {
+            return JSON.stringify(key) + ": " + helper(x[key], prefix)
+          })
+          .join(", ") +
+        "}"
+      )
+    }
+
+    return "undefined"
+  }
+
+  return helper(decycle(x), prefix)
 }
 
 module.exports = stringify
--- src/stringify.test.js
+++ src/stringify.test.js
@@ -1,16 +1,28 @@
 const stringify = require("./stringify.js")
 
 test("tests that standard values can be stringified", () => {
+  function double(x) {
+    return x * 2
+  }
+
   const pairs = [
-    [234, "234"],
-    ["foobar", `"foobar"`],
-    [true, "true"],
+    [-2.3, "-2.3"],
+    [-Infinity, "-Infinity"],
+    ["foo", `"foo"`],
+    [[2, 3, 4], "[2, 3, 4]"],
+    [{ hello: "world" }, `{"hello": "world"}`],
+    [0, "0"],
+    [1, "1"],
+    [2.3, "2.3"],
+    [double, "function double(x) {\n    return x * 2;\n  }"],
     [false, "false"],
-    [undefined, undefined],
+    [Infinity, "Infinity"],
+    [NaN, "NaN"],
     [null, "null"],
-    [x => x, undefined],
-    [[2, 3, 4], "[2,3,4]"],
-    [{ hello: "world" }, `{"hello":"world"}`],
+    [Symbol.for("Hello, world!"), "Symbol(Hello, world!)"],
+    [true, "true"],
+    [undefined, "undefined"],
+    [x => x, "x => x"],
   ]
 
   pairs.forEach(pair => {
@@ -22,13 +34,13 @@ test("tests that objects and arrays with circular references can be stringified"
   const arr = [2, 3, 4]
   arr.push([[arr]])
   arr.push([5, 6, [arr]])
-  const arrTrue = `[2,3,4,[["<reference to \\"/\\">"]],[5,6,["<reference to \\"/\\">"]]]`
+  const arrTrue = `[2, 3, 4, [["<reference to \\"/\\">"]], [5, 6, ["<reference to \\"/\\">"]]]`
   expect(stringify(arr)).toBe(arrTrue)
 
   const obj = { hello: { to: { the: "world" } } }
   obj.hello.to.copy = obj.hello.to
   obj["self"] = { x: { y: { z: obj.hello.to } } }
   obj["arr"] = { values: [2, obj.hello.to, 3, 4] }
-  const objTrue = `{"hello":{"to":{"the":"world","copy":"<reference to \\"/hello/to\\">"}},"self":{"x":{"y":{"z":{"the":"world","copy":"<reference to \\"/hello/to\\">"}}}},"arr":{"values":[2,{"the":"world","copy":"<reference to \\"/hello/to\\">"},3,4]}}`
+  const objTrue = `{"hello": {"to": {"the": "world", "copy": "<reference to \\"/hello/to\\">"}}, "self": {"x": {"y": {"z": {"the": "world", "copy": "<reference to \\"/hello/to\\">"}}}}, "arr": {"values": [2, {"the": "world", "copy": "<reference to \\"/hello/to\\">"}, 3, 4]}}`
   expect(stringify(obj)).toBe(objTrue)
 })
