9034759e268fa5098704f9a3e65301e918b8986b
jrc03c <jrc03c@pm.me>
2023-06-27 09:40:34 -0500

    ...

--- src/kebabify-bin.js
+++ src/kebabify-bin.js
@@ -1,13 +1,25 @@
 #!/usr/bin/env node
+const { execSync } = require("node:child_process")
 const color = require("@jrc03c/bash-colors")
 const kebabify = require("./kebabify")
 
 const { bright } = color.fx
-const { magenta } = color.fg
+const { magenta, yellow } = color.fg
 
 if (process.argv.length < 3) {
   console.log(`\n  The syntax is: ${bright(magenta("kebabify <text>"))}\n`)
   process.exit(0)
 }
 
-console.log(kebabify(process.argv[2]))
+const out = kebabify(process.argv[2])
+console.log(out)
+
+try {
+  execSync(`echo "${out}" | xsel -b`, { encoding: "utf8" })
+} catch (e) {
+  console.log(
+    yellow(
+      "NOTE: This tool can copy the resulting text to the clipboard automatically if you have `xsel` installed!"
+    )
+  )
+}
