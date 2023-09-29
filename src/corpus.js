b257c7364e750ba4aec5c54709316eb0d5b1c108
jrc03c <jrc03c@pm.me>
2023-09-28 20:22:11 -0500

    ...

--- src/corpus.js
+++ src/corpus.js
@@ -6,60 +6,100 @@ const {
   isString,
   isUndefined,
   log,
+  set,
+  sort,
 } = require("@jrc03c/js-math-tools")
 
 const { safePathJoin, safeWrite } = require("./helpers/safe-fs")
 const shortHash = require("./helpers/short-hash")
+const strip = require("./helpers/strip")
 
 function log10(x) {
-  return log(x) / log(10)
+  return divide(log(x), log(10))
 }
 
 class Corpus {
+  #id = undefined
+
+  static clean(text) {
+    return strip(text)
+      .split(/\s/g)
+      .filter(v => v.length > 0)
+      .join(" ")
+  }
+
+  static getWordSet(text) {
+    return sort(set(Corpus.clean(text).split(" ")))
+  }
+
   constructor(docs, cacheDir) {
     assert(
       isArray(docs) && docs.every(doc => isString(doc)),
-      "The first argument passed into the `Corpus` constructor must be an array of strings representing the list of documents that belong in the corpus!",
+      "The first argument passed into the `Corpus` constructor must be an array of strings representing the list of documents that belong in the corpus!"
     )
 
     assert(
       isUndefined(cacheDir) || isString(cacheDir),
-      "The second argument passed into the `Corpus` constructor must be undefined or a string representing a filesystem path to a cache directory (or, in the browser, a root key for use in `localStorage`)!",
+      "The second argument passed into the `Corpus` constructor must be undefined or a string representing a filesystem path to a cache directory (or, in the browser, a root key for use in `localStorage`)!"
     )
 
-    this.docs = docs
+    this.docs = set(docs)
     this.cache = { words: {}, docs: {} }
     this.cacheDir = cacheDir
+  }
+
+  get id() {
+    if (!this.#id) {
+      throw new Error(
+        "A corpus only receives its ID after indexing its documents! Please call the `index` method before requesting the corpus ID."
+      )
+    }
+
+    return this.#id
+  }
+
+  set id(value) {
+    if (this.#id) {
+      throw new Error(
+        'The "id" property of the corpus has already been set and is now read-only!'
+      )
+    }
+
+    assert(
+      isString(value),
+      'The "id" property of a corpus can only have string values assigned to it!'
+    )
+
+    this.#id = value
   }
 
   async index(progress) {
     assert(
       isUndefined(progress) || isFunction(progress),
-      "The argument passed into the `index` method must be undefined or a function that receives a value between 0 and 1!",
+      "The argument passed into the `index` method must be undefined or a function that receives a value between 0 and 1!"
     )
 
-    for (const i in this.docs) {
+    const cleanedDocs = []
+    const docIds = []
+
+    for (let i = 0; i < this.docs.length; i++) {
       if (progress) {
-        progress(i / (this.docs.length - 1))
+        progress(i / this.docs.length)
       }
 
       const doc = this.docs[i]
-      const docId = await shortHash(doc)
+      const cleaned = Corpus.clean(doc)
+      cleanedDocs.push(cleaned)
 
-      const words = strip(doc)
-        .split(/\s/g)
-        .filter(v => v.length > 0)
+      const docId = await shortHash(cleaned)
+      docIds.push(docId)
 
-      const cleaned = words.join(" ")
-      this.cache.docs[docId] = { wordCount: words.length }
+      const words = cleaned.split(" ")
+      this.cache.docs[docId] = { wordCount: words.length, mostCommonWord: null }
+      let mostCommonWord = null
+      let mostCommonWordCount = 0
 
-      for (const j in words) {
-        if (progress) {
-          progress((i + j / (words.length - 1)) / (this.docs.length - 1))
-        }
-
-        const word = words[j]
-
+      for (const word of words) {
         if (!this.cache.words[word]) {
           this.cache.words[word] = {}
         }
@@ -69,34 +109,54 @@ class Corpus {
         }
 
         this.cache.words[word][docId]++
+
+        if (this.cache.words[word][docId] > mostCommonWordCount) {
+          mostCommonWordCount = this.cache.words[word][docId]
+          mostCommonWord = word
+        }
       }
+
+      this.cache.docs[docId].mostCommonWord = mostCommonWord
     }
 
+    this.id = await shortHash(cleanedDocs.join("\n\n"))
+
     if (this.cacheDir) {
-      safeWrite(safePathJoin(this.cacheDir, corpusId + ".json"), this.cache)
+      safeWrite(safePathJoin(this.cacheDir, this.id + ".json"), this.cache)
     }
 
     progress(1)
-    return this
+    return docIds
   }
 
-  async tf(word, doc) {
-    const docId = await shortHash(doc)
-    return this.cache.words[word][docId] / this.cache.docs[docId].wordCount
+  async tf(word, docId) {
+    if (!this.cache.words[word]) {
+      return 0
+    }
+
+    if (!this.cache.docs[docId]) {
+      throw new Error(
+        "The document you passed into the `tf` method is not part of the corpus and was therefore not indexed!"
+      )
+    }
+
+    const doc = this.cache.docs[docId]
+    const wordFreq = this.cache.words[word][docId]
+    const mostCommonWordFreq = this.cache.words[doc.mostCommonWord][docId]
+    return 0.5 + (0.5 * wordFreq) / mostCommonWordFreq
   }
 
   async idf(word) {
     try {
-      return log10(
-        this.docs.length / Object.keys(this.cache.words[word]).length,
-      )
+      const docCount = Object.keys(this.cache.words[word]).length
+      return log10((this.docs.length - docCount) / docCount)
     } catch (e) {
       return NaN
     }
   }
 
-  async tfidf(word, doc) {
-    return (await this.tf(word, doc)) * (await this.idf(word))
+  async tfidf(word, docId) {
+    return (await this.tf(word, docId)) * (await this.idf(word))
   }
 }
 
