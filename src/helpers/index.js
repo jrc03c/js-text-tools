9f44210b98e7589c4f7a6b0ff6d56ec7abe7c89e
jrc03c <jrc03c@pm.me>
2023-09-30 18:02:55 -0500

    ...

---
+++ src/helpers/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  convertObjectToTypedArray: require("./convert-object-to-typed-array"),
+  convertTypedArrayToObject: require("./convert-typed-array-to-object"),
+  hash: require("./hash"),
+  punctuation: require("./punctuation"),
+  replaceAll: require("./replace-all"),
+  shortHash: require("./short-hash"),
+  strip: require("./strip"),
+}
